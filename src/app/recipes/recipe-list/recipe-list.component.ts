13f3a12f55b1ef166f04170c446c26f5c169825e
kurkure15 <ankury15@gmail.com>
2019-04-23 22:04:19 +0530

    first

---
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from "@angular/core";
+import { Recipe } from "../recipe.model";
+@Component({
+  selector: "app-recipe-list",
+  templateUrl: "./recipe-list.component.html",
+  styleUrls: ["./recipe-list.component.css"]
+})
+export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe( //here we instantiated the constructor created in recipe.model.ts
+      "Chhole Bhature",
+      "Served with Cholle",
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/9/97/Bhatura.jpg/800px-Bhatura.jpg"),
+      new Recipe( //here we instantiated the constructor created in recipe.model.ts
+        "Chhole Bhature",
+        "Served with Cholle",
+        "https://upload.wikimedia.org/wikipedia/commons/thumb/9/97/Bhatura.jpg/800px-Bhatura.jpg")
+      ];
+      
+  constructor() {}
+
+  ngOnInit() {}
+}
